a0d516d3ade8c59ba91db9d3e77f5f341a4a6e91
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-08-31 16:50:02 +0200

    added check for already submitted form

--- backend/routes/tastings.js
+++ backend/routes/tastings.js
@@ -8,6 +8,13 @@ router.get('/', verify, async (req, res) => {
 });
 
 router.post('/', verify, async (req, res) => {
+    const user_id = req.body[0].user_id
+    const event_id = req.body[0].event_id
+    const result = await knex('tastings').where('event_id', event_id).andWhere('user_id', user_id);
+    if (result.length !== 0){
+        res.json({ message: "Form has already been submitted"})
+        return
+    }
     await knex('tastings').insert(req.body);
     res.json({ message: "OK"})
 });
