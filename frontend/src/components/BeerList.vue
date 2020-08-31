03faa0419fe6c7288cf7dbd0d681b415a3c44e9c
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-08-31 15:59:06 +0200

    Updated component

--- frontend/src/components/BeerList.vue
+++ frontend/src/components/BeerList.vue
@@ -1,26 +1,58 @@
 <template>
   <v-container flex>
-    <v-card v-for="beer in beerList" :key="beer.position"
+    <v-card v-for="beer in beerList" :key="beer.nr"
       width="100%"
       height="50"
       class="ma-2"
       elevation="4"
+      :dark="beer.checked"
+      @click="toggle(beer)"
       >
       <v-card-text>
-        {{beer.position}}. {{beer.name}}
+        {{beer.nr}}. {{beer.name}}
       </v-card-text>
     </v-card>
+    <v-btn
+      block
+      color="primary"
+      class="mr-4"
+      @click="close"
+    >
+      Close
+    </v-btn>
+    <v-btn
+      block
+      color="primary"
+      class="mr-4"
+      @click="finish"
+    >
+      Finish
+    </v-btn>
   </v-container>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
-  computed: {
-    ...mapState(['beerList'])
+  data: () => ({
+    beerList: []
+  }),
+  methods: {
+    toggle(beer) {
+      beer.checked = !beer.checked
+      localStorage.setItem('beerList', JSON.stringify(this.beerList))
+    },
+    close() {
+      this.$store.dispatch('updateEventState', 'CLOSED')
+    },
+    finish() {
+      this.$store.dispatch('updateEventState', 'FINISHED')
+      localStorage.removeItem('beerList')
+      localStorage.removeItem('eventDetails')
+      this.$router.push('/')
+    }
   },
-  mounted() {
-    this.$store.dispatch('loadBeerListForCurrentEvent')
+  created() {
+    this.beerList = JSON.parse(localStorage.getItem('beerList'))
   }
 }
 </script>
